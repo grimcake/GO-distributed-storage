4817dd3544aeb268bcb867b167a5a2852d370009
grimcake <994459124@qq.com>
2019-08-17 11:04:28 -0700

    update

---
+++ db/file.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	mydb "filestore-server/db/mysql"
+)
+
+// TableFile : 文件表对应的一些字段
+type TableFile struct {
+	FileHash string
+	FileName sql.NullString
+	FileSize sql.NullInt64
+	FileAddr sql.NullString
+}
+
+// OnFileUploadFinished : 文件上传完成，保存meta
+func OnFileUploadFinished(filehash string, filename string,
+	filesize int64, fileaddr string) bool {
+	stmt, err := mydb.DBConn().Prepare(
+		"insert ignore into tbl_file (`file_sha1`,`file_name`,`file_size`," +
+			"`file_addr`,`status`) values (?,?,?,?,1)")
+	if err != nil {
+		fmt.Println("Failed to prepare statement, err:" + err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	ret, err := stmt.Exec(filehash, filename, filesize, fileaddr)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	if rf, err := ret.RowsAffected(); nil == err {
+		if rf <= 0 {
+			fmt.Printf("File with hash:%s has been uploaded before", filehash)
+		}
+		return true
+	}
+	return false
+}
